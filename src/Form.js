e1ed1b70bda4c6607872bfc6d4c8d17fd513cad2
Franklin Chian <franklin.chian@berkeley.edu>
2019-08-07 23:23:58 -0700

    submit

--- src/Form.js
+++ src/Form.js
@@ -1,100 +1,122 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/Feedback';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-export default class Form extends React.Component {
-  state = {
-    firstName: "dog",
-    lastName: "",
-    email: "",
-    feedback: "",
-  };
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.blue,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
-  change = e => {
-    this.setState({[e.target.name]: e.target.value
-    });
-  };
+export default function SignIn() {
+  const classes = useStyles();
 
-  onSubmit = e => {
-    e.preventDefault();
-    /*if (this.state.firstName==''||this.state.lastName==''||
-    this.state.email==''||this.state.feedback=='') {
-      alert('All fields required');
-      return false;
-    }*/
-    if (this.handleValidation()){
-      console.log(this.state);
-      alert("Form submitted");
-    }else{
-      alert("You need to fill out all fields.")
-    }
-  }
-
-  handleValidation(){
-    var fName = this.state.firstName;
-    var lName = this.state.lastName;
-    var email = this.state.email;
-    var feedback = this.state.feedback;
-    var validForm = true;
-    if (fName ==""||lName==""||email==""||feedback=="") {
-      validForm = false;
-    }
-    return validForm
-  }
-  render() {
-    return (
-      <Container component="main" maxWidth="xs">
+  return (
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <form>
-        <input
-          name = 'firstName'
-          placeholder='First name'
-          value = {this.state.firstName}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name = 'lastName'
-          placeholder='Last name'
-          value = {this.state.lastName}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name = 'email'
-          placeholder='Email'
-          value = {this.state.email}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <input
-          name = 'feedback'
-          placeholder='feedback'
-          value = {this.state.feedback} onChange={e => e.target.value}
-          onChange={e => this.change(e)}
-        />
-        <br />
-        <Button variant="contained" size='large' color='primary'onClick={e => this.onSubmit(e)}>Submit</Button>
-      </form>
-      <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                {"Call us at"}
-              </Link>
-            </Grid>
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Feedback
+        </Typography>
+        <form className={classes.form} >
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="First Name"
+            label="First Name"
+            name="First Name"
+            autoComplete="First Name"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="Last Name"
+            label="Last Name"
+            type="Last Name"
+            id="Last Name"
+            autoComplete="Last Name"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="Email"
+            label="John@example.com"
+            type="Email"
+            id="Email"
+            autoComplete="Email"
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            multiline={true}
+            inputProps={{maxLength: 500}}
+            name="Feedback"
+            label="Message"
+            type="Feedback"
+            id="Feedback"
+            autoComplete="Feedback"
+          />
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+          <Grid container>
             <Grid item>
-              <Link href="#" variant="body2">
-                {"Email us @"}
+              <Link href='#' variant="body2">
+                {"Questions? Email us."}
               </Link>
             </Grid>
           </Grid>
-       </Container>
-    );
-  }
+        </form>
+      </div>
+    </Container>
+  );
 }
